da72d3a5bbc9c7175094fdf4b90bc58e24e06480
RfqiAlan <rifqialan46475@gmail.com>
2026-06-03 17:21:58 +0800

    fix: remove unused React import in App.tsx

--- centerdrive-frontend/src/App.tsx
+++ centerdrive-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Sidebar } from './components/Sidebar';
 import { MainPanel } from './components/MainPanel';
